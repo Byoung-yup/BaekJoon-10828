c35d4ae196a0b072689c7c6c53ab0df27c22e20a
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-01-19 23:55:04 +0900

    commit

--- BaekJoon#10828/main.swift
+++ BaekJoon#10828/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+let n = Int(readLine()!)!
+var Stack = [Int]()
 
+for _ in 0..<n {
+    
+    let input = readLine()!.split(separator: " ")
+    
+    if input[0] == "push" { Stack.append(Int(input[1])!) }
+    
+    else if input[0] == "pop" {
+        if Stack.isEmpty { print("-1") }
+        else { print(Stack.removeLast()) }
+    }
+    
+    else if input[0] == "top" {
+        if Stack.isEmpty { print("-1") }
+        else { print(Stack[Stack.count - 1]) }
+    }
+    
+    else if input[0] == "size" { print(Stack.count) }
+    
+    else if input[0] == "empty" {
+        if Stack.isEmpty { print(1) }
+        else { print(0) }
+    }
+}
